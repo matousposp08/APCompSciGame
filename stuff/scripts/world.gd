5bee7acfc3f3d6fe9936a206c5a1b6461d86c22c
EvanC22 <evanchin0322@gmail.com>
2024-01-05 14:15:45 -0500

    multiplayer base
    stuff should still work till i figure out authority and whatnot

--- stuff/scripts/world.gd
+++ stuff/scripts/world.gd
@@ -1,5 +1,11 @@
-extends Node3D
+extends Node
 
+@onready var main_menu = $"CanvasLayer/Main Menu"
+@onready var address_entry = $"CanvasLayer/Main Menu/MarginContainer/VBoxContainer/AddressEntry"
+
+const player = preload("res://scenes/player/player.tscn")
+const PORT = 9999
+var enet_peer = ENetMultiplayerPeer.new()
 
 var paused = false
 
@@ -27,3 +33,24 @@ func _process(_delta):
 			pause_game()
 		else:
 			unpause_game()
+
+
+func _on_host_pressed():
+	main_menu.hide()
+	
+	enet_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	
+	add_player(multiplayer.get_unique_id())
+	multiplayer.peer_connected.connect(add_player)
+
+func _on_join_pressed():
+	main_menu.hide()
+	
+	enet_peer.create_client("localhost", PORT)
+	multiplayer.multiplayer_peer = enet_peer
+
+func add_player(peer_id):
+	var player = player.instantiate()
+	player.name = str(peer_id)
+	add_child(player)
