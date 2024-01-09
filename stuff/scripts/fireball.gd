d8384cb9427a24aeba4dc5562ec0c103b90f3f28
finsld <142837936+dVillarosa287@users.noreply.github.com>
2024-01-09 13:46:37 -0500

    fireball: remove unneeded code

--- stuff/scripts/fireball.gd
+++ stuff/scripts/fireball.gd
@@ -5,7 +5,6 @@ const SPEED = 30
 @onready var mesh = $MeshInstance3D
 @onready var ray = $RayCast3D
 @onready var particles = $GPUParticles3D
-var y = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Area3D/CollisionShape3D.disabled = true
@@ -13,9 +12,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	y *= -2
 	position += transform.basis * Vector3(0,0,-SPEED) * delta
-	position.y -= y
 	add_to_group("player")
 	if ray.is_colliding():
 		$Area3D/CollisionShape3D.disabled = false
