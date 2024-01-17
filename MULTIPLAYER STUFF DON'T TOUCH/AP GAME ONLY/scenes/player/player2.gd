74d0c881e35276d6f671f170f1053b5c69716af9
EvanC22 <evanchin0322@gmail.com>
2024-01-17 09:56:10 -0500

    added movement like running and stuff to multiplayer player

--- MULTIPLAYER STUFF DON'T TOUCH/AP GAME ONLY/scenes/player/player2.gd
+++ MULTIPLAYER STUFF DON'T TOUCH/AP GAME ONLY/scenes/player/player2.gd
@@ -1,12 +1,24 @@
 extends CharacterBody3D
 
+@export var sprint_enabled: bool = true
+@export var crouch_enabled: bool = true
+
 @onready var camera = $Camera3D
 @onready var anim_player = $AnimationPlayer
+@onready var body = $body
+@onready var collision = $CollisionShape3D
 
-const SPEED = 10.0
+var speed = 6.0
+var sprint_speed = 8.0
+var crouch_speed: float = 3.0
 const JUMP_VELOCITY = 10.0
 
 var gravity = 20.0
+
+var state: String = "normal"  
+var camera_fov_extents: Array[float] = [75.0, 85.0] 
+var base_player_y_scale: float = 1.0
+var crouch_player_y_scale: float = 0.75
 
 func _enter_tree():
 	set_multiplayer_authority(str(name).to_int())
@@ -16,6 +28,10 @@ func _ready():
 	
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	camera.current = true
+	
+func _process(delta: float) -> void:
+	handle_movement_input(delta)
+	update_camera(delta)
 	
 func _unhandled_input(event):
 	if not is_multiplayer_authority(): return
@@ -57,8 +73,48 @@ func move_character(delta: float) -> void:
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+func handle_movement_input(delta: float) -> void:
+	if Input.is_action_pressed("move_sprint") and !Input.is_action_pressed("move_crouch") and sprint_enabled:
+		if !$crouch_roof_detect.is_colliding(): 
+			enter_sprint_state(delta)
+	elif Input.is_action_pressed("move_crouch") and !Input.is_action_pressed("move_sprint") and crouch_enabled:
+		enter_crouch_state(delta)
+	else:
+		if !$crouch_roof_detect.is_colliding():
+			enter_normal_state(delta)
+			
+func enter_sprint_state(delta: float) -> void:
+	state = "sprinting"
+	speed = sprint_speed
+	camera.fov = lerp(camera.fov, camera_fov_extents[1], 10 * delta)
+
+func enter_crouch_state(delta: float) -> void:
+	state = "crouching"
+	speed = crouch_speed
+	apply_crouch_transform(delta)
+	
+func enter_normal_state(delta: float) -> void:
+	state = "normal"
+	speed = 6.0
+	reset_transforms(delta)
+
+func update_camera(delta: float) -> void:
+	match state:
+		"sprinting":
+			camera.fov = lerp(camera.fov, camera_fov_extents[1], 10 * delta)
+		"normal":
+			camera.fov = lerp(camera.fov, camera_fov_extents[0], 10 * delta)
+
+func apply_crouch_transform(delta: float) -> void:
+	body.scale.y = lerp(body.scale.y, crouch_player_y_scale, 10 * delta)
+	collision.scale.y = lerp(collision.scale.y, crouch_player_y_scale, 10 * delta)
+	
+func reset_transforms(delta: float) -> void:
+	body.scale.y = lerp(body.scale.y, base_player_y_scale, 10 * delta)
+	collision.scale.y = lerp(collision.scale.y, base_player_y_scale, 10 * delta)
