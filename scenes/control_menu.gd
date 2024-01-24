958d8446ec7228216a93089fbd6296e41cc83f43
matousposp08 <154290575+matousposp08@users.noreply.github.com>
2024-01-24 10:33:47 -0500

    make volume controls

--- scenes/control_menu.gd
+++ scenes/control_menu.gd
@@ -1,9 +1,9 @@
 extends Node2D
-
+var master_bus = AudioServer.get_bus_index("Master")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$HSlider.value = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,3 +11,12 @@ func _process(delta):
 	$Prup.rotate(0.01)
 	$Prup2.rotate(0.01)
 	$Prup3.rotate(0.01)
+
+
+
+func _on_h_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(master_bus, value)
+	if value == -30:
+		AudioServer.set_bus_mute(master_bus, true)
+	else:
+		AudioServer.set_bus_mute(master_bus, false)
