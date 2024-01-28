8421debf1b004b6f1517e9284d9473927a907b37
finsld <142837936+dVillarosa287@users.noreply.github.com>
2024-01-28 15:46:43 -0500

    player attack: tweak magic visuals

--- stuff/scripts/lightning.gd
+++ stuff/scripts/lightning.gd
@@ -17,7 +17,7 @@ func _process(delta):
 	position += transform.basis * Vector3(0,0,-SPEED) * delta
 	spark()
 	#if ray.is_colliding():
-	#	$Area3D/CollisionShape3D.disabled = false
+	#	$Area3D/CollisionShape3D.disablexd = false
 	#	mesh.visible = false
 	#	await get_tree().create_timer(1,0).timeout
 	#	queue_free()
