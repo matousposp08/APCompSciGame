79d088a17cdfbdfd243193bc1ee6d7f17719652b
Puhalenthi Ramesh Vidhya <57294477+Puhalenthi@users.noreply.github.com>
2024-01-29 18:15:30 -0500

    player: make cursor stay in game

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -48,7 +48,7 @@ func _enter_tree():
 
 func _ready() -> void:
 	if not is_multiplayer_authority(): return
-	
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	$head/crowbar/Area3D/CollisionShape3D.disabled = false
 	parts["camera"].current = true
 
